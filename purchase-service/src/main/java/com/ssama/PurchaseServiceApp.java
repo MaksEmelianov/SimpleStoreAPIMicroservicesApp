ba6b421f52d21a29b638cd5ea58c54c8f869d5f4
Maksim Emelyanov <mr.maksimemelyanov@ya.ru>
2023-03-21 16:13:16 +0500

    Initial commit

---
+++ purchase-service/src/main/java/com/ssama/PurchaseServiceApp.java
@@ -0,0 +1,11 @@
+package com.ssama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseServiceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(PurchaseServiceApp.class, args);
+    }
+}
